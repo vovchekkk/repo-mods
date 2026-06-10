44e888ed78c1d323f2f13e5ca1908cc827ae091c
Vladimir Gavrikov <vlgavrikov06@gmail.com>
2026-06-10 15:56:07 +0500

    update v3

--- update.bat
+++ update.bat
@@ -1,3 +1,31 @@
 @echo off
-bash update.sh
+setlocal
+
+REM текущая папка = repo-mods
+cd /d %~dp0
+
+REM -----------------------------
+REM 📦 обновляем репозиторий
+REM -----------------------------
+git pull
+
+REM -----------------------------
+REM 📍 поднимаемся в корень игры
+REM (repo-mods лежит внутри REPO)
+REM -----------------------------
+cd ..
+
+REM -----------------------------
+REM 🧹 очищаем BepInEx\plugins
+REM -----------------------------
+if exist "BepInEx\plugins" (
+    rmdir /s /q "BepInEx\plugins"
+)
+
+REM -----------------------------
+REM 📦 копируем моды
+REM -----------------------------
+xcopy "repo-mods\*" "." /E /I /Y
+
+endlocal
 pause
