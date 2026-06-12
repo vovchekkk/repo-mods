977170d77040742fd5ff14b569ad5b1253553ce7
Vladimir Gavrikov <vlgavrikov06@gmail.com>
2026-06-12 12:40:40 +0500

    new update

--- update.bat
+++ update.bat
@@ -4,9 +4,12 @@ setlocal
 cd /d %~dp0
 
 REM -----------------------------
-REM 📦 обновляем репозиторий
+REM 📦 синхронизируем репозиторий
 REM -----------------------------
-git pull
+git fetch origin
+git checkout main
+git reset --hard origin/main
+git clean -fd
 
 cd ..
 
